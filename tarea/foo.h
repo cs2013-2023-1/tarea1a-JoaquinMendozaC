c852d9afbfcdde47b64aa2c166d626d28e404ebe
JoaquinMendozaC <129106933+JoaquinMendozaC@users.noreply.github.com>
2023-04-09 14:33:00 -0500

    Update foo.h

--- tarea/foo.h
+++ tarea/foo.h
@@ -5,21 +5,21 @@ using namespace std;
 class Matriz2D{
 friend Matriz2D t(Matriz2D&);
 friend ostream& operator<<(ostream&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator-(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator*(const Matriz2D&, const Matriz2D&);
-friend Matriz2D operator+(const Matriz2D&, float);
-friend Matriz2D operator-(const Matriz2D&, float);
-friend Matriz2D operator*(const Matriz2D&, float);
-friend Matriz2D operator/(const Matriz2D&, float);
+friend Matriz2D operator+(const Matriz2D& m1, const Matriz2D& m2);
+friend Matriz2D operator-(const Matriz2D& m1, const Matriz2D& m2);
+friend Matriz2D operator*(const Matriz2D& m1, const Matriz2D& m2);
+friend Matriz2D operator+(const Matriz2D& m, float n);
+friend Matriz2D operator-(const Matriz2D& m, float n);
+friend Matriz2D operator*(const Matriz2D& m, float n);
+friend Matriz2D operator/(const Matriz2D& m, float n);
 public:
     Matriz2D();
-    Matriz2D(int);
-    Matriz2D(int, int);
-    Matriz2D(const Matriz2D&);
-    Matriz2D(Matriz2D&&);
+    Matriz2D(int n);
+    Matriz2D(int n, int m);
+    Matriz2D(const Matriz2D& m);
+    Matriz2D(Matriz2D&& m);
 
-    float get(int, int);
+    float get(int i, int j);
     int getFilas();
     int getColumnas();
 private:
